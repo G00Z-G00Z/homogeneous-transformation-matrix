d03c824df24052005df2b7dfd92f7bc8e2ef01ec
G00Z_G00Z <edygomezgg4@gmail.com>
2023-02-18 13:37:33 -0600

    Made function that almost work

--- calculations.py
+++ calculations.py
@@ -24,6 +24,23 @@ def get_initial_arm_coordinates(angle, length):
 
 # %%
 
+# Function that returns a 3x3 homogeneous transformation matrix given an angle in degrees and a length
+
+
+def get_homogeneous_transformation_matrix(angle, length):
+    # Get the rotation matrix
+    rot = rot2d(angle)
+
+    # Get the coordinates of the end of the arm
+    end = np.array([length, 0]).T
+
+    # Get the homogeneous transformation matrix
+
+    return np.block([[rot, end.reshape(2, 1)], [np.zeros(2), 1]])
+
+
+# %%
+
 # Calculates final poisition of the end effector given the angles of the two arms and the length of the arms
 # a1, a2: length of the arms
 # theta1, theta2: angles of the arms
@@ -31,18 +48,13 @@ def get_initial_arm_coordinates(angle, length):
 
 
 def calculate_global_position_homogenous(a1, a2, theta1, theta2):
-    # Find T1G which is the homogeneous transformation matrix from frame 1 to frame G
-    rotation1 = rot2d(theta1)
-    translation1 = get_initial_arm_coordinates(theta1, a1)
-
-    # Find T2G which is the homogeneous transformation matrix from frame 2 to frame G
-    rotation2 = rot2d(theta2)
-    translation2 = get_initial_arm_coordinates(theta2, a2)
+    T1G = get_homogeneous_transformation_matrix(theta1, a1)
+    T2G = get_homogeneous_transformation_matrix(theta2, a2)
 
     # Find the global position of the end effector
-    global_point = rotation1 @ translation2 + translation1
+    global_point = T1G @ T2G
 
-    return global_point
+    return global_point[0:2, 2]
 
 
 def calculate_global_position_not_homogenous(a1, a2, theta1, theta2):
@@ -67,7 +79,7 @@ a2 = a1  # cm
 theta1 = 30  # degrees
 theta2 = 45  # degrees
 
-global_point = calculate_global_position_not_homogenous(a1, a2, theta1, theta2)
+global_point = calculate_global_position_homogenous(a1, a2, theta1, theta2)
 
 print(global_point)
 
